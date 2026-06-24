cddf57f0b445ff5c87c597da2675fd8b296ee4b3
hyunehub <kbh581126@gmail.com>
2026-06-24 14:29:30 +0900

    Add shared contest type

---
+++ src/types/contest.ts
@@ -0,0 +1,11 @@
+export interface Contest {
+  id: string;
+  title: string;
+  host: string;
+  period: string;
+  posterImage: string;
+  ageBadgeText: string;
+  ageBadgeColor: string;
+  statusBadgeText: string;
+  statusBadgeColor: string;
+}
